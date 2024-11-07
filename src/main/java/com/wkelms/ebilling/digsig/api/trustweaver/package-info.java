2a718b893920b4882b6efa2f18da59b9dabf9a5a
vijrams <mailrvijay@gmail.com>
2024-11-07 15:21:08 -0600

    Delete src/main/java/com/wkelms/ebilling/digsig/api directory

--- src/main/java/com/wkelms/ebilling/digsig/api/trustweaver/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Signature switch
- * 
- */
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.trustweaver.com/tsswitch", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.wkelms.ebilling.digsig.api.trustweaver;
